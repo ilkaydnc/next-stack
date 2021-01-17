9306eea41658e2f096ad808d61a3890445f86f2e
ilkaydnc <ilkaydinc1@gmail.com>
2021-01-17 22:13:05 +0300

    Add theme and ThemeProvider to Layout component

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,8 +1,34 @@
-import React from 'react'
-import { createGlobalStyle } from 'styled-components'
+import React, { ReactNode } from 'react'
+import { createGlobalStyle, ThemeProvider } from 'styled-components'
 
-interface Props {
-  children: JSX.Element
+const palette = {
+  primary: {
+    light: '#35bdf2',
+    main: '#0442bf',
+    dark: '#056cf2',
+    contrastText: '#f2f2f2',
+  },
+  gray: {
+    50: '#F9FAFB',
+    100: '#F3F4F6',
+    200: '#E5E7EB',
+    300: '#D1D5DB',
+    400: '#9CA3AF',
+    500: '#6B7280',
+    600: '#4B5563',
+    700: '#374151',
+    800: '#1F2937',
+    900: '#111827',
+  },
+}
+
+const theme = {
+  palette,
+  border: {
+    light: palette.gray[50],
+    main: palette.gray[50],
+    dark: palette.gray[50],
+  },
 }
 
 export const GlobalStyle = createGlobalStyle`  
@@ -24,12 +50,16 @@ export const GlobalStyle = createGlobalStyle`
   }
 `
 
-const Layout = ({ children }: Props): JSX.Element => {
+interface Props {
+  children: ReactNode
+}
+
+const Layout = ({ children }: Props): ReactNode => {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <GlobalStyle />
       {children}
-    </>
+    </ThemeProvider>
   )
 }
 
